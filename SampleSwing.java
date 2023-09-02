5a4322773a9a7264963d6ecfe570ffc48ff73a25
{dmmago} <{xx3503.cr@gmail.com}>
2023-09-02 12:03:46 +0900

    画像表示されない

---
+++ SampleSwing.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class SampleSwing {
+private static JFrame frame ;
+private static ImageIcon image;
+private static JLabel label;
+    public static void main(String[] args) {
+        frame = new JFrame();
+        image = new ImageIcon("test.jpg");
+        label = new JLabel(image);
+
+        frame.getContentPane().add(label);
+
+        frame.getContentPane().setLayout(null);
+        label.setBounds(0, 0, 640, 480);
+
+        frame.setTitle("SampleSwing");
+        frame.setSize(640, 480);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+    }
+}
