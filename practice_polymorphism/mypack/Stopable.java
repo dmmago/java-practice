1ede45f9185fb4c217fd78a6e9a023f411b88e5d
{dmmago} <{xx3503.cr@gmail.com}>
2023-09-06 23:57:06 +0900

    practice

---
+++ practice_polymorphism/mypack/Stopable.java
@@ -0,0 +1,4 @@
+package mypack;
+public interface Stopable {
+  void stop();
+}
---
+++ practice_polymorphism/mypack/Vehicle.java
@@ -0,0 +1,9 @@
+package mypack;
+public abstract class Vehicle {
+  protected int crewNum;
+//protectedサブクラスからアクセス可能
+  
+
+
+  public abstract void showCrewNum();
+}
