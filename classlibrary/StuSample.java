357f02ef4fca5632cf1ad84272547a9547f3ee4b
{dmmago} <{xx3503.cr@gmail.com}>
2023-09-07 22:17:21 +0900

    practice

---
+++ classlibrary/StuSample.java
@@ -0,0 +1,10 @@
+public class StuSample{
+  public static void main(String[] args){
+    Student stu1 = new Student("加藤");
+    Student stu2 = new Student("加藤");
+
+    boolean result = stu1.equals(stu2);
+    System.out.println(result);
+    System.out.println(stu1);
+  }
+}
